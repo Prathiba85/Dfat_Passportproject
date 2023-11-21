2a24cd68fad1799d4c34329011547340960f9917
[Prathiba] <prathiba.sankararaj@gmail.com>
2023-11-21 11:18:41 +1100

    committed

--- src/test/java/AppHooks/Hooks.java
+++ src/test/java/AppHooks/Hooks.java
@@ -4,8 +4,8 @@ package AppHooks;
 
 import java.util.Properties;
 
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
+//import org.openqa.selenium.OutputType;
+//import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
 import com.qa.factory.DriverFactory;
@@ -14,7 +14,7 @@ import com.qa.util.ConfigReader;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 
-import io.cucumber.java.Scenario;
+//import io.cucumber.java.Scenario;
 
 public class Hooks {
 
