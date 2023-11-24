a0a014c5ccab5a18196d301071091dc38bd223ac
[Prathiba] <prathiba.sankararaj@gmail.com>
2023-11-24 16:49:59 +1100

    committed

--- src/test/java/AppHooks/Hooks.java
+++ src/test/java/AppHooks/Hooks.java
@@ -40,7 +40,7 @@ public class Hooks {
 		driver = driverFactory.init_driver(browserName);
 		driver.manage().window().maximize();
 	}
-	
+	/*
 	@After(order = 0)
 	public void teardownandScreenshot(Scenario scenario)
 	{
@@ -62,9 +62,9 @@ public class Hooks {
 			byte[] sourcepath=((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
 			scenario.attach(sourcepath, "image/png", screenshotName);
 		}
-	}
+	}*/
 	
-	/*The below code must be used for git push
+	//The below code must be used for git push
 	@After(order = 0)
 	public void teardownandScreenshot()
 	{
@@ -72,12 +72,9 @@ public class Hooks {
 		
 	}
 	@After(order =1)
-	public void teardown(Scenario scenario)
+	public void teardown()
 	{
-		if(scenario.isFailed())
-		{
-			
-		}
-	}*/
+		
+	}
 
 }
